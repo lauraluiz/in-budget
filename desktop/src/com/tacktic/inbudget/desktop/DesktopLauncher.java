98d2bdefc45b17c90220714a65603f9eacd5f3d1
Laura Luiz <laura.luiz@commercetools.de>
2014-11-21 18:33:37 +0100

    Build game structure.

---
+++ desktop/src/com/tacktic/inbudget/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tacktic.inbudget.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tacktic.inbudget.InBudget;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InBudget(), config);
+	}
+}
